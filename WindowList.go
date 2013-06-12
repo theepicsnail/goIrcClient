e0604d9ff9f9f25e288f9fb25066b1a5654451f2
Steven <theepicsnail@gmail.com>
2013-06-11 21:07:20 -0400

    Added Window struct
    Windows represent separate dialogs/messages/channels in the irc client
    Ultimately they should have their own logs, names, action flags, etc...

--- WindowList.go
+++ WindowList.go
@@ -1,31 +1,63 @@
 package main
 import (
     gc "code.google.com/p/goncurses"
-  //  "fmt"
 )
+
+type Window struct {
+    name string
+    history []string
+}
+
 type WindowList struct {
-    windowNames []string
+    windows []Window
     display gc.Window
     selected int
 }
+
 func NewWindowList(window gc.Window) *WindowList {
     w := new(WindowList)
     w.display = window
-    w.createWindow("win1")
-    w.createWindow("win2")
     return w
 }
 
-func (w *WindowList) createWindow(name string) {
-    w.windowNames = append(w.windowNames, name)
-    w.selectWindowById(len(w.windowNames)-1)
-}
-
-func (w *WindowList) selectWindowById(num int) {
+func (w *WindowList) SelectWindowById(num int) Window {
+    //Unselect the old window
     w.display.Move(w.selected,0)
     w.display.ClearToEOL()
-    w.display.MovePrint(w.selected,0," %s ",w.windowNames[w.selected])
+    w.display.MovePrint(w.selected,0," %s ",w.windows[w.selected].name)
+
     w.selected = num
-    w.display.MovePrint(w.selected, 0,"[%s]", w.windowNames[w.selected])
+    selWin := w.windows[w.selected]
+
+    w.display.MovePrint(w.selected, 0,"[%s]", selWin.name)
     w.display.Refresh()
+    return selWin
+}
+
+func (wlist *WindowList) SelectWindowByName(name string) Window {
+    //Finds a window by name, if it's not found, create it.
+    id := wlist.findWindowByName(name)
+
+    if id == -1 {
+        id = wlist.createWindow(name)
+    }
+
+    return wlist.SelectWindowById(id)
+}
+
+func (wList *WindowList) findWindowByName(name string) int {
+    for n, win := range(wList.windows) {
+        if win.name == name {
+            return n
+        }
+    }
+    return -1
+}
+
+func (wlist *WindowList) createWindow(name string) int {
+    win := new(Window)
+    win.name = name
+    win.history = make([]string, 16)
+    wlist.windows = append(wlist.windows, *win)
+    return len(wlist.windows) - 1
 }
